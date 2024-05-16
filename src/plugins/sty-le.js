b4440d32565ef43ca148eff46c033948e3ced4f3
Markus Walther <dr.marikos@gmail.com>
2024-05-16 11:43:13 +0200

    allow over-the-network loading of shareable <sty-le> via new 'src' attribute + adapt demo to that

--- src/plugins/sty-le.js
+++ src/plugins/sty-le.js
@@ -6,23 +6,35 @@
   // custom element definition
   class StyLe extends HTMLElement {
     // when connected to DOM...
-    connectedCallback() {
+    async connectedCallback() {
       // extract own id="<styleIdentifier>"
       let { id } = this;
       // try to get cached style sheet for <styleIdentifier>
       let styleSheet = cache.get(id);
 
       // cache match?
-      if (styleSheet) {
-        // yes, use it inside our ShadowDOM domain (or under document.body, if there is none)
-        this.getRootNode().adoptedStyleSheets.push(styleSheet);
-      } else {
+      if (!styleSheet) {
         // no cache match: prepare a new style sheet
         styleSheet = new CSSStyleSheet();
-        // fill it with the *immediately preceding inline <style>* content
-        styleSheet.replaceSync(this.previousElementSibling.textContent);
-        // and cache the newly minted style sheet under <styleIdentifier>
-        cache.set(id, styleSheet);
+        // determine style content
+        let src = this.getAttribute("src");
+        // promise to yield a style sheet
+        let promise = new Promise(async resolve => {
+          let content = src
+            ? /* fetch from assumed .css file */ await (await fetch(src)).text()
+            : /* *immediately preceding inline <style>* content */ this
+                .previousElementSibling.textContent;
+          // fill it with the style content
+          styleSheet.replaceSync(content);
+          // yield the style sheet
+          resolve(styleSheet);
+        });
+        // cache the promise under the <id> key
+        cache.set(id, promise);
+      }
+      if (styleSheet) {
+        // yes, use it inside our ShadowDOM domain (or under document.body, if there is none)
+        this.getRootNode().adoptedStyleSheets.push(await styleSheet);
       }
       // remove ourselves from DOM, now that the work is done!
       this.remove();
