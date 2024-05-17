61d1aeacf473b2f9e7208d0931ef796cb402685a
Markus Walther <dr.marikos@gmail.com>
2024-05-17 14:12:45 +0200

    add accordion component to demo, showing off <link>-based style loading, delegated click handling, and external script loading for component hydration

---
+++ accordion.js
@@ -0,0 +1,17 @@
+customElements.whenDefined("sig-nal").then( ({hydrate}) =>
+  hydrate(['acc-ordion'], {
+   'accordion': {'@click':
+       ({e}) => {
+       const buttons = e.composedPath().filter(node => node.tagName === 'BUTTON');
+       if (!buttons.length) return;
+       const button = buttons[0];
+       const slot = button.parentNode.nextElementSibling;
+       const isOpen = button.classList.contains('open');
+       button.classList[isOpen ? 'remove' : 'add']('open');
+       button.title = isOpen ? 'close' : 'open';
+       slot.classList[isOpen ? 'remove' : 'add']('show');
+       }
+     }
+  })
+);
+
